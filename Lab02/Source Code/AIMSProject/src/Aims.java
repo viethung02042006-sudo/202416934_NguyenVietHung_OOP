a6a72ad184e8e5c0206494f13f23a1dd5f36157e
Nguyen Viet Hung <viethung02042006@gmail.com>
2026-04-03 15:59:35 +0700

    Nop source code phan 5

---
+++ Lab02/Source Code/AIMSProject/src/Aims.java
@@ -0,0 +1,11 @@
+public class Aims {
+    public static void main(String[] args) {
+        // TODO: Auto-generated method stub
+
+        // Cart anOrder = new Cart();
+
+        // Thử nghiệm tạo DVD
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("SpiderMan-BrandNewDay", "Animation", "Roger Allers", 87, 19.95f);
+        System.out.println("DVD Title: " + dvd1.getTitle());
+    }
+}
