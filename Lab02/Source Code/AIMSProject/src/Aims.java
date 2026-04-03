2327fe2cbb660ae67338bad362ae5dd7e07ec914
Nguyen Viet Hung <viethung02042006@gmail.com>
2026-04-03 17:13:17 +0700

    Nop bai hoan thanh den phan 8 bao gom ca reading assignment va answer.txt

--- Lab02/Source Code/AIMSProject/src/Aims.java
+++ Lab02/Source Code/AIMSProject/src/Aims.java
@@ -1,11 +1,17 @@
 public class Aims {
     public static void main(String[] args) {
-        // TODO: Auto-generated method stub
+        // Chạy thử hàm khởi tạo số 1 (chỉ có title)
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("Interstella");
 
-        // Cart anOrder = new Cart();
+        // Chạy thử hàm khởi tạo số 2 (category, title, cost)
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Animation", "Aladdin", 18.99f);
 
-        // Thử nghiệm tạo DVD
-        DigitalVideoDisc dvd1 = new DigitalVideoDisc("SpiderMan-BrandNewDay", "Animation", "Roger Allers", 87, 19.95f);
-        System.out.println("DVD Title: " + dvd1.getTitle());
+        // Chạy thử hàm khởi tạo số 4 (đầy đủ thông tin)
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Star Wars", "Sci-fi", "George Lucas", 124, 24.95f);
+
+        // In thử ra màn hình để kiểm tra
+        System.out.println("DVD 1 Title: " + dvd1.getTitle());
+        System.out.println("DVD 2 Title: " + dvd2.getTitle() + " - Cost: " + dvd2.getCost());
+        System.out.println("DVD 3 Director: " + dvd3.getDirector());
     }
 }
--- Lab02/Source Code/AIMSProject/src/DigitalVideoDisc.java
+++ Lab02/Source Code/AIMSProject/src/DigitalVideoDisc.java
@@ -5,7 +5,29 @@ public class DigitalVideoDisc {
     private int length;
     private float cost;
 
-    // Constructor
+    // --- CÁC HÀM KHỞI TẠO (CONSTRUCTORS) - PHẦN 8 ---
+
+    // 1. Khởi tạo bằng tiêu đề (title)
+    public DigitalVideoDisc(String title) {
+        this.title = title;
+    }
+
+    // 2. Khởi tạo bằng danh mục, tiêu đề và giá (category, title, cost)
+    public DigitalVideoDisc(String category, String title, float cost) {
+        this.category = category;
+        this.title = title;
+        this.cost = cost;
+    }
+
+    // 3. Khởi tạo bằng đạo diễn, danh mục, tiêu đề và giá (director, category, title, cost)
+    public DigitalVideoDisc(String director, String category, String title, float cost) {
+        this.director = director;
+        this.category = category;
+        this.title = title;
+        this.cost = cost;
+    }
+
+    // 4. Khởi tạo bằng tất cả thuộc tính
     public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
         this.title = title;
         this.category = category;
@@ -14,10 +36,25 @@ public class DigitalVideoDisc {
         this.cost = cost;
     }
 
-    //Getter
-    public String getTitle() { return title; }
-    public String getCategory() { return category; }
-    public String getDirector() { return director; }
-    public int getLength() { return length; }
-    public float getCost() { return cost; }
+    // --- CÁC HÀM LẤY DỮ LIỆU (GETTERS) - PHẦN 7 ---
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public float getCost() {
+        return cost;
+    }
 }
