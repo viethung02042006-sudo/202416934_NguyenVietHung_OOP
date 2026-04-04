37ab46613eb5765e524e3ade8bb95adaec5e6dc6
Nguyen Viet Hung <viethung02042006@gmail.com>
2026-04-04 15:47:04 +0700

    Hoan thanh phan 10 va them anh ket qua chay code

--- Lab02/Source Code/AIMSProject/src/Aims.java
+++ Lab02/Source Code/AIMSProject/src/Aims.java
@@ -1,29 +1,29 @@
 public class Aims {
     public static void main(String[] args) {
-        // 1. Tạo một giỏ hàng mới
+        // Tạo một giỏ hàng mới (Create a new cart)
         Cart anOrder = new Cart();
 
-        // 2. Tạo các đối tượng DVD mới và thêm vào giỏ hàng
-        // Sử dụng các Constructor khác nhau đã học ở phần 8
+        // Tạo các đối tượng DVD và thêm chúng vào giỏ hàng
+        // Create new dvd objects and add them to the cart
+
+        // DVD 1: The Lion King
         DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King",
                 "Animation", "Roger Allers", 87, 19.95f);
         anOrder.addDigitalVideoDisc(dvd1);
 
+        // DVD 2: Star Wars
         DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars",
-                "Science Fiction", "George Lucas", 124, 24.95f);
+                "Science Fiction", "George Lucas", 87, 24.95f);
         anOrder.addDigitalVideoDisc(dvd2);
 
+        // DVD 3: Aladdin
         DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladdin",
                 "Animation", 18.99f);
         anOrder.addDigitalVideoDisc(dvd3);
 
-        // 3. In tổng chi phí của các mặt hàng trong giỏ hàng
-        System.out.print("Total Cost is: ");
+        // In tổng chi phí của các mặt hàng trong giỏ hàng
+        // Print total cost of the items in the cart
+        System.out.println("Total Cost is: ");
         System.out.println(anOrder.totalCost());
-
-        // 4. Thử nghiệm phương thức xóa đĩa khỏi giỏ hàng
-        // Sau khi xóa dvd2, tổng tiền phải giảm đi 24.95
-        anOrder.removeDigitalVideoDisc(dvd2);
-        System.out.println("Total Cost after removal: " + anOrder.totalCost());
     }
 }
--- Lab02/Source Code/AIMSProject/src/Cart.java
+++ Lab02/Source Code/AIMSProject/src/Cart.java
@@ -1,21 +1,26 @@
 public class Cart {
     public static final int MAX_NUMBERS_ORDERED = 20;
-    private DigitalVideoDisc itemsOrdered[] =
-            new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
+    private DigitalVideoDisc itemsOrdered[] = new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
     private int qtyOrdered = 0;
 
-    //   thêm DVD vào giỏ hàng
+    // Thêm DVD vào giỏ hàng
     public void addDigitalVideoDisc(DigitalVideoDisc disc) {
         if (qtyOrdered < MAX_NUMBERS_ORDERED) {
             itemsOrdered[qtyOrdered] = disc;
             qtyOrdered++;
-            System.out.println("The disc has been added"); // Thông báo theo yêu cầu
+            System.out.println("The disc has been added");
+
+            // Nếu sau khi thêm mà giỏ hàng chạm mốc 20
+            if (qtyOrdered == MAX_NUMBERS_ORDERED) {
+                System.out.println("The cart is almost full");
+            }
         } else {
-            System.out.println("The cart is almost full"); // Thông báo khi đầy
+            // Nếu giỏ hàng đã đầy (>= 20)
+            System.out.println("The cart is almost full");
         }
     }
 
-    //  xóa DVD khỏi giỏ hàng
+    // Xóa DVD khỏi giỏ hàng
     public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
         int indexFound = -1;
         for (int i = 0; i < qtyOrdered; i++) {
@@ -26,11 +31,11 @@ public class Cart {
         }
 
         if (indexFound != -1) {
-            // Dịch chuyển các phần tử phía sau lên để lấp chỗ trống
+            // Dịch chuyển các phần tử phía sau lên trước
             for (int i = indexFound; i < qtyOrdered - 1; i++) {
                 itemsOrdered[i] = itemsOrdered[i + 1];
             }
-            itemsOrdered[qtyOrdered - 1] = null;
+            itemsOrdered[qtyOrdered - 1] = null; // Xóa tham chiếu cuối cùng
             qtyOrdered--;
             System.out.println("The disc has been removed");
         } else {
@@ -38,13 +43,11 @@ public class Cart {
         }
     }
 
-    //   tính tổng tiền
+    // Tính tổng tiền các DVD trong giỏ
     public float totalCost() {
         float total = 0;
         for (int i = 0; i < qtyOrdered; i++) {
-            if (itemsOrdered[i] != null) {
-                total += itemsOrdered[i].getCost(); // Sử dụng getter đã tạo ở phần 7
-            }
+            total += itemsOrdered[i].getCost();
         }
         return total;
     }
