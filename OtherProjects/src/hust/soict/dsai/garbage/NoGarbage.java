6aea9f605789029618a5f7a343a7a0ae728904db
Nguyen Viet Hung <viethung02042006@gmail.com>
2026-04-19 00:09:36 +0700

    create a new package hust.soict.globalict.garbage for ICT or hust.soict.dsai.garbage for DS-AI

---
+++ OtherProjects/src/hust/soict/dsai/garbage/NoGarbage.java
@@ -0,0 +1,25 @@
+package hust.soict.dsai.garbage;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class NoGarbage {
+    public static void main(String[] args) {
+        String filename = "test.exe";
+        byte[] inputBytes = { 0 };
+        long startTime, endTime;
+
+        try {
+            inputBytes = Files.readAllBytes(Paths.get(filename));
+            startTime = System.currentTimeMillis();
+            StringBuilder outputStringBuilder = new StringBuilder();
+            for (byte b : inputBytes) {
+                outputStringBuilder.append((char)b); // Dùng StringBuilder để tối ưu
+            }
+            endTime = System.currentTimeMillis();
+            System.out.println("Processing time (StringBuilder): " + (endTime - startTime) + "ms");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
