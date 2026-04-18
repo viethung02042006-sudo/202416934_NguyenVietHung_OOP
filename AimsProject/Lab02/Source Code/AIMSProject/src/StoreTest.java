249c22878a1f14352e1f6a6bb85e04efe655898a
Nguyen Viet Hung <viethung02042006@gmail.com>
2026-04-18 23:07:55 +0700

    - Successfully created the Store class with an array to manage available DVDs. - Implemented addDVD() and removeDVD() methods. - Verified functionality using StoreTest class.

---
+++ AimsProject/Lab02/Source Code/AIMSProject/src/StoreTest.java
@@ -0,0 +1,18 @@
+public class StoreTest {
+    public static void main(String[] args) {
+        Store myStore = new Store();
+
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King");
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars");
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladdin");
+
+        // Test thêm đĩa
+        myStore.addDVD(dvd1);
+        myStore.addDVD(dvd2);
+        myStore.addDVD(dvd3);
+
+        // Test xóa đĩa
+        myStore.removeDVD(dvd2); // Xóa đĩa có trong kho
+        myStore.removeDVD(new DigitalVideoDisc("Avengers")); // Xóa đĩa không có trong kho
+    }
+}
