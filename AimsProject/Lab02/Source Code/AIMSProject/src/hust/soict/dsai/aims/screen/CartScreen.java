40a18c1769dde46528f560b25e296818dc9c9b61
Nguyen Viet Hung <viethung02042006@gmail.com>
2026-06-02 17:24:35 +0700

    Hoàn thành phần 6: Triển khai lớp CartScreen tích hợp JavaFX vào Swing

---
+++ AimsProject/Lab02/Source Code/AIMSProject/src/hust/soict/dsai/aims/screen/CartScreen.java
@@ -0,0 +1,44 @@
+package hust.soict.dsai.aims.screen;
+
+import java.io.IOException;
+import javax.swing.JFrame;
+import javafx.application.Platform;
+import javafx.embed.swing.JFXPanel;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import hust.soict.dsai.aims.cart.Cart;
+
+public class CartScreen extends JFrame {
+    private Cart cart;
+
+    public CartScreen(Cart cart) {
+        super();
+
+        this.cart = cart;
+
+        JFXPanel fxPanel = new JFXPanel();
+        this.add(fxPanel);
+
+        this.setTitle("Cart");
+        this.setSize(1024, 768);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    FXMLLoader loader = new FXMLLoader(getClass()
+                            .getResource("/hust/soict/dsai/aims/screen/cart.fxml"));
+                    CartScreenController controller = new CartScreenController(cart);
+                    loader.setController(controller);
+                    Parent root = loader.load();
+                    fxPanel.setScene(new Scene(root));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
---
+++ AimsProject/Lab02/Source Code/AIMSProject/src/hust/soict/dsai/aims/screen/CartScreenController.java
@@ -0,0 +1,12 @@
+package hust.soict.dsai.aims.screen;
+
+import hust.soict.dsai.aims.cart.Cart;
+
+public class CartScreenController {
+    private Cart cart;
+
+    public CartScreenController(Cart cart) {
+        super();
+        this.cart = cart;
+    }
+}
