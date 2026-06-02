9f51667c98ffcc2e72eb58baafda5bfb58cdc82a
Nguyen Viet Hung <viethung02042006@gmail.com>
2026-06-02 17:38:49 +0700

    Hoàn thành phần 8: Triển khai ẩn hiện nút bấm theo sản phẩm được chọn bằng ChangeListener

--- AimsProject/Lab02/Source Code/AIMSProject/src/hust/soict/dsai/aims/screen/CartScreenController.java
+++ AimsProject/Lab02/Source Code/AIMSProject/src/hust/soict/dsai/aims/screen/CartScreenController.java
@@ -3,9 +3,13 @@ package hust.soict.dsai.aims.screen;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.Button;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import hust.soict.dsai.aims.cart.Cart;
 import hust.soict.dsai.aims.media.Media;
+import hust.soict.dsai.aims.media.Playable;
 
 public class CartScreenController {
     private Cart cart;
@@ -22,6 +26,12 @@ public class CartScreenController {
     @FXML
     private TableColumn<Media, Float> colMediaCost;
 
+    @FXML
+    private Button btnPlay;
+
+    @FXML
+    private Button btnRemove;
+
     public CartScreenController(Cart cart) {
         super();
         this.cart = cart;
@@ -33,5 +43,28 @@ public class CartScreenController {
         colMediacategory.setCellValueFactory(new PropertyValueFactory<Media, String>("category"));
         colMediaCost.setCellValueFactory(new PropertyValueFactory<Media, Float>("cost"));
         tblMedia.setItems(this.cart.getItemsOrdered());
+
+        btnPlay.setVisible(false);
+        btnRemove.setVisible(false);
+
+        tblMedia.getSelectionModel().selectedItemProperty().addListener(
+            new ChangeListener<Media>() {
+                @Override
+                public void changed(ObservableValue<? extends Media> observable, Media oldValue, Media newValue) {
+                    if (newValue != null) {
+                        updateButtonBar(newValue);
+                    }
+                }
+            }
+        );
+    }
+
+    void updateButtonBar(Media media) {
+        btnRemove.setVisible(true);
+        if (media instanceof Playable) {
+            btnPlay.setVisible(true);
+        } else {
+            btnPlay.setVisible(false);
+        }
     }
 }
